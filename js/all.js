0852d483c63649757e575289641efeb8740bc8e4
RitaLing1021 <106168868+RitaLing1021@users.noreply.github.com>
2024-08-29 17:53:38 +0800

    修改表頭選項變色

--- js/all.js
+++ js/all.js
@@ -1,17 +1,35 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('mainNavbar');
-    const dropdownMenus = document.querySelectorAll('.dropdown-menu');
-    if (window.scrollY > 0) {
-      navbar.style.backgroundColor = navbar.offsetHeight > 80 ? '#004E42' : '#004E42';
-      dropdownMenus.forEach(menu => {
-        if (navbar.offsetHeight > 80) {
-          menu.style.backgroundColor = '#004E42'; // 設置下拉選單背景色
-        } else {
-          menu.style.backgroundColor = '#004E42'; // 保持透明
-        }
-      });
-    } else {
-      navbar.style.backgroundColor = 'transparent';
-      dropdownMenus.forEach(menu => menu.style.backgroundColor = 'transparent'); // 當回到頂部時，下拉選單也保持透明
-    }
-  });
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('mainNavbar');
+  const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+  const navLinks = document.querySelectorAll('.nav-link'); // 取得所有導航連結
+  const dropDown = document.querySelectorAll('.dropdown-item'); // 取得所有導航連結
+  if (window.scrollY > 0) {
+    navbar.style.backgroundColor = navbar.offsetHeight > 80 ? '#004E42' : '#004E42';
+    navLinks.forEach(link => {
+      if (navbar.offsetHeight > 80) {
+        link.style.color = '#004E42'; // 當高度超過 80px 時，連結文字顏色變為白色
+      } else {
+        link.style.color = '#FFF'; // 高度未超過 80px 時，連結文字顏色變為 #004E42
+      }
+    });
+    dropDown.forEach(link => {
+      if (navbar.offsetHeight > 80) {
+        link.style.color = '#004E42'; // 當高度超過 80px 時，連結文字顏色變為白色
+      } else {
+        link.style.color = '#FFF'; // 高度未超過 80px 時，連結文字顏色變為 #004E42
+      }
+    });
+    dropdownMenus.forEach(menu => {
+      if (navbar.offsetHeight > 80) {
+        menu.style.backgroundColor = '#004E42'; // 設置下拉選單背景色
+      } else {
+        menu.style.backgroundColor = '#004E42'; // 保持透明
+      }
+    });
+  } else {
+    navbar.style.backgroundColor = 'transparent';
+    navLinks.forEach(link => {link.style.color = '#004E42';}); // 連結文字顏色變為 #004E42
+    dropDown.forEach(link => {link.style.color = '#004E42';}); // 連結文字顏色變為 #004E42
+    dropdownMenus.forEach(menu => menu.style.backgroundColor = 'transparent'); // 當回到頂部時，下拉選單也保持透明
+  }
+});
