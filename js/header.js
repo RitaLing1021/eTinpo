f5f946c51ea0ec2954662922567131682c05b95b
RitaLing1021 <106168868+RitaLing1021@users.noreply.github.com>
2024-09-19 11:05:56 +0800

    關於我們下拉、產品類別的了解更多按鈕都先隱藏、修復首頁BANNER

--- js/header.js
+++ js/header.js
@@ -57,8 +57,11 @@ header =/*html*/`
           <li class="nav-item">
             <a class="nav-link" aria-current="page" href="technologies.html">富邦技術</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="about.html">關於我們</a>
+          </li>
           <!-- 下拉選單 -->
-          <li class="nav-item dropdown">
+          <!--<li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               關於我們
             </a>
@@ -69,7 +72,7 @@ header =/*html*/`
               <li><a class="dropdown-item" href="#">環境</a></li>
               <li><a class="dropdown-item" href="#">研發(特別研發對象)</a></li>
             </ul>
-          </li>
+          </li>-->
           <li class="nav-item">
             <a class="nav-link" href="product-features.html">產品總覽</a>
           </li>
