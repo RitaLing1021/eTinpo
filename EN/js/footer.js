dd539c899286ac41b0539b58219ab5ed542156c0
RitaLing1021 <ritaling1021@gmail.com>
2024-11-06 01:43:14 +0800

    英文版放置完成

---
+++ EN/js/footer.js
@@ -0,0 +1,32 @@
+footer =/*html*/`
+<footer>
+    <div class="container d-flex flex-md-row flex-column justify-content-around align-items-center p-3">
+        <div><img src="https://etinpo.com.tw/image/footer-logo.png" alt="eTinpo-footerlogo"></div>
+        <div class="text-center text-md-start py-3 footerLocation">
+            Phone / +886-6-505-1179<br>
+            Email / info@etinpo.com.tw <br>
+            Monday to Friday 9:30 A.M. to 5 P.M.<br>
+            2F., No. 10, Chuangye Rd., Xinshi Dist., Tainan City 74144
+        </div>
+        <div class="text-center text-md-end">
+        <!--<div class="footerBtn">
+            <a href="#"><img src="../image/icon-fb.png" alt="facebook"></a>
+            <a href="#"><img src="../image/icon-line.png" alt="Line"></a>
+            <a href="#"><img src="../image/icon-ig.png" alt="ig"></a>
+</div>-->
+            <div class="footerLink">
+                <a href="#">Technologies</a>｜
+                <a href="#">About us</a>｜
+                <a href="#">Products</a>｜
+                <a href="#">Contact</a>｜
+                <a href="#">Our Partners</a>
+            </div>
+        </div>
+    </div>
+    <div class="text-center copyRight">
+        Copyright © 2024 ForeBond Advanced Material Co., Ltd. All Rights Reserved.
+    </div>
+</footer>
+`;
+
+document.currentScript.insertAdjacentHTML('beforebegin', footer);
